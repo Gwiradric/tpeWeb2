ef192f8c2f20e999f177c90ceb2d686dcd6337f9
Federico Fuhr <ffuhr@alumnos.exa.unicen.edu.ar>
2019-10-26 14:51:24 -0300

    updated db

--- db/db.sql
+++ db/db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 20-10-2019 a las 19:04:35
+-- Tiempo de generación: 26-10-2019 a las 19:50:49
 -- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+-- Versión de PHP: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `genres` (
   `id_genre` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `description` varchar(100) NOT NULL
+  `description` varchar(600) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -39,13 +39,13 @@ CREATE TABLE `genres` (
 --
 
 INSERT INTO `genres` (`id_genre`, `name`, `description`) VALUES
-(1, 'Terror', 'El horror o terror es un género literario que se define por la sensación que causa: miedo'),
-(5, 'Aventuras', 'Suceso extraño o poco frecuente que vive una persona, especialmente el que es emocionante'),
-(12, 'Comedia', 'Da alegría con momentos de drama'),
-(35, 'Ciencia ficción', 'Es la denominación de uno de los géneros derivados de la literatura de ficción'),
-(37, 'Acción', 'Este tipo de película son de alta tensión y contienen persecuciones y muchas peleas'),
-(38, 'Dramáticas', 'Son películas serias, con personajes y situaciones muy realistas, parecidas a la vida cotidiana'),
-(39, 'Suspenso', 'Estas películas contienen un contenido relacionado con asesinatos o con el crimen organizado');
+(1, 'Horror', 'The distinction between horror and terror is a standard literary and psychological concept applied especially to Gothic and horror fiction. Terror is usually described as the feeling of dread and anticipation that precedes the horrifying experience. By contrast, horror is the feeling of revulsion that usually follows a frightening sight, sound, or otherwise experience.'),
+(5, 'Adventures', 'An adventure is an event or series of events that happens outside the course of the protagonist\'s ordinary life, usually accompanied by danger, often by physical action. Adventure stories almost always move quickly, and the pace of the plot is at least as important as characterization, setting and other elements of a creative work.'),
+(12, 'Comedy', 'Comedy may be divided into multiple genres based on the source of humor, the method of delivery, and the context in which it is delivered.  These classifications overlap, and most comedians can fit into multiple genres. For example, deadpan comics often fall into observational comedy, or into black comedy or blue comedy to contrast the morbidity, or offensiveness of the joke with a lack of emotion. '),
+(35, 'Science fiction', 'Is a genre of speculative fiction that typically deals with imaginative and futuristic concepts such as advanced science and technology, time travel, parallel universes, fictional worlds, space exploration, and extraterrestrial life. It has been called the '),
+(37, 'Action', ' Is a film genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, and frantic chases. Action films tend to feature a resourceful hero struggling against incredible odds, which include life-threatening situations, a villain, or a pursuit which usually concludes in victory for the hero (though a small number of films in this genre have ended in the victory for the villain instead). '),
+(38, 'Drama', 'The drama genre is strongly based in a character, or characters, that are in conflict at a crucial moment in their lives. Most dramas revolve around families and often have tragic or painful resolutions.'),
+(39, 'Suspense', 'Suspense is a feeling of fascination and excitement mixed with apprehension, tension, and anxiety developed from an unpredictable, mysterious, and rousing source of entertainment. The term most often refers to an audience\'s perceptions in a dramatic work. Suspense is not exclusive to fiction.');
 
 -- --------------------------------------------------------
 
@@ -57,24 +57,25 @@ CREATE TABLE `movies` (
   `id_movie` int(11) NOT NULL,
   `id_genre` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `description` varchar(100) NOT NULL,
+  `description` varchar(600) NOT NULL,
   `year` int(11) NOT NULL,
-  `rating` int(11) NOT NULL
+  `rating` int(11) NOT NULL,
+  `img` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `movies`
 --
 
-INSERT INTO `movies` (`id_movie`, `id_genre`, `name`, `description`, `year`, `rating`) VALUES
-(1, 1, 'Silent Hill', 'Pelicula basasa en el famoso juego de terror', 2005, 4),
-(2, 1, 'Resident Evil', 'Un peligroso virus se ha propagado intencionalmente por la Corporación Umbrella', 2000, 10),
-(3, 5, 'Tarzan', 'Película estadounidense de animación tradicional de 1999. Se basa en Tarzán de Edgar Rice Burroughs', 1996, 20),
-(12, 12, 'Hercules', 'Es una película de comedia estadounidense protagonizada por Dwayne Johnson', 2015, 50),
-(20, 35, 'Blade Runner 2049', 'Es una película neo-noir estadounidense', 2017, 8),
-(22, 39, 'Shutter Island', 'Leo y Mark Ruffalo interpretan a dos detectives que investigan la desaparición de un paciente', 2010, 87),
-(23, 37, 'Géminis', 'Henry Brogan, un asesino a sueldo ya mayor para este tipo de trabajo, decide retirarse.', 2019, 29),
-(24, 38, 'Wounds', 'Una serie de sucesos misteriosos comienzan a suceder cuando un camarero contesta una llamada', 2019, 62);
+INSERT INTO `movies` (`id_movie`, `id_genre`, `name`, `description`, `year`, `rating`, `img`) VALUES
+(2, 1, 'Resident Evil', 'Resident Evil is a 2002 action horror film written and directed by Paul W. S. Anderson. The film stars Milla Jovovich, Michelle Rodriguez, Eric Mabius, James Purefoy, Martin Crewes, and Colin Salmon. It is the first installment in the Resident Evil film series, which is loosely based on the video game series of the same name.', 2002, 49, 2),
+(3, 5, 'Tarzan', 'Tarzan, a fictional character created by Edgar Rice Burroughs, first appeared in the 1912 novel Tarzan of the Apes, and then in twenty-three sequels. The character proved immensely popular and quickly made the jump to other media, first and most notably to comics and film. This article concerns Tarzan\'s appearance in film and other non-print media.', 1929, 21, 3),
+(12, 12, 'Hercules', 'Hercules is a 1997 American animated musical fantasy comedy film produced by Walt Disney Feature Animation for Walt Disney Pictures. The 35th Disney animated feature film and the eighth animated film produced during the Disney Renaissance, the film was directed by Ron Clements and John Musker. The film is loosely based on the legendary hero Heracles (known in the film by his Roman name, Hercules), the son of Zeus, in Greek mythology. ', 1997, 61, 4),
+(20, 35, 'Blade Runner 2049', 'Thirty years after the events of the first film, a new blade runner, LAPD Officer K, unearths a long-buried secret that has the potential to plunge what’s left of society into chaos. K’s discovery leads him on a quest to find Rick Deckard, a former LAPD blade runner who has been missing for 30 years.', 2017, 67, 5),
+(22, 39, 'Shutter Island', 'Shutter Island is a 2010 American neo-noir psychological thriller film directed by Martin Scorsese and written by Laeta Kalogridis, based on Dennis Lehane\'s 2003 novel of the same name. Leonardo DiCaprio stars as U.S. Marshal Edward \"Teddy\" Daniels who is investigating a psychiatric facility on Shutter Island after one of the patients goes missing.', 2010, 87, 6),
+(23, 37, 'Géminis', 'Lucia and Daniel have three children: Ezequiel, who has gone to live in Spain, Jeremias and, their youngest, Meme. Lucia, a pretentious woman, believes she can control everything according to the criteria and habits of a typical wealthy Argentinean family. She is blind to the fact that Jeremias and Meme are conducting an illicit relationship under her own roof. Ezequiel arrives home from Spain with his fiancée, Montse, to get married, much to the great joy of his proud parents. The young couple\'s presence reveals the fragility of the order of things that had seemed so perfect. Love becomes a s', 2010, 29, 7),
+(24, 38, 'Wounds', 'Wounds is a 2019 psychological horror film written and directed by Babak Anvari and starring Armie Hammer, Dakota Johnson, and Zazie Beetz. The film is based on the novella The Visible Filth by Nathan Ballingrud.', 2019, 62, 8),
+(28, 1, 'Silent Hill', 'Silent Hill is a 2006 French-Canadian psychological horror film directed by Christophe Gans and written by Roger Avary, Gans, and Nicolas Boukhrief. The film is an adaptation of Konami\'s 1999 video game Silent Hill. It stars Radha Mitchell, Sean Bean, Laurie Holden, Deborah Kara Unger, Kim Coates, Tanya Allen, Alice Krige, and Jodelle Ferland.', 2006, 78, 1);
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `username`, `password`) VALUES
-(1, 'fedefuhr', '$2y$10$dXO0i0WT3BCQo.bbMovKUekc9Du4Iw3LINi6e4CLnrrVEhy7imyWi');
+(1, 'fedefuhr', '$2y$10$dXO0i0WT3BCQo.bbMovKUekc9Du4Iw3LINi6e4CLnrrVEhy7imyWi'),
+(2, 'pepe', '$2y$10$eBKLILx6VNYubaN5vls96uPnEgKB9K/nWLyK7VF/VHoJVGRNFklxe');
 
 --
 -- Índices para tablas volcadas
@@ -126,19 +128,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de la tabla `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `id_movie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_movie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
